c46240b457e185817ecd17206111da02175f03a2
Haukur Rosinkranz <hauxir@gmail.com>
2018-12-18 13:35:58 +0100

    use vuejs for frontend

--- app/frontend/app.js
+++ app/frontend/app.js
@@ -1,158 +1,152 @@
-function init_elements() {
-  $(".loadingwrapper").hide();
-  $(".filename")
-    .html("")
-    .hide();
-  $("form").hide();
-  $("table").hide();
-  $(".progress-bar").width(0);
-  $("tbody").html("");
-  $(".videolink").hide();
-  $(".progress").hide();
-  $(".loadingtext").html("Loading");
-  $(".peernum").html("");
-  $(".peernum").hide();
-  var id = window.setTimeout(function() {}, 0);
-  while (id--) {
-    window.clearTimeout(id); // will do nothing if no timeout with id is present
+(function() {
+  function get_hash(magnet_link) {
+    var hash_start = magnet_link.indexOf("btih:") + 5;
+    var hash_end = magnet_link.indexOf("&");
+    if (hash_end == -1) return magnet_link.substr(hash_start);
+    return magnet_link.substr(hash_start, hash_end - hash_start);
   }
-}
 
-function get_hash(magnet_link) {
-  console.log(magnet_link);
-  var hash_start = magnet_link.indexOf("btih:") + 5;
-  var hash_end = magnet_link.indexOf("&");
-  if (hash_end == -1) return magnet_link.substr(hash_start);
-  return magnet_link.substr(hash_start, hash_end - hash_start);
-}
+  var rbmixin = {
+    props: ["params"],
+    methods: {
+      navigate: function(path) {
+        router.navigate(path);
+      }
+    }
+  };
 
-var router = new Navigo(window.location.origin);
-router
-  .on({
-    "search/:searchterm": function(params) {
-      init_elements();
-      $("table").show();
-      $(".loadingwrapper").show();
-      $("form").hide();
-      $.get("/api/search/" + params.searchterm, function(data) {
-        $("tbody").html("");
-        data.results.forEach(function(result) {
-          var tr = $("<tr><td>" + result.title + "</a></td></tr>");
-          $(tr).click(function() {
-            router.navigate(
-              "/magnet/" +
-                encodeURIComponent(encodeURIComponent(result.magnet)) +
-                "/"
-            );
-          });
-          $("tbody").append(tr);
-        });
-        $(".loadingwrapper").hide();
-      }).fail(function() {
-        $(".loadingwrapper").hide();
-      });
+  Vue.component("loading-spinner", {
+    props: ["heading", "subheading", "progress"],
+    template: "#loading-spinner-template",
+    updated: function() {
+      var progress_el = this.$refs.progress;
+      progress_el && (progress_el.style.width = this.progress * 100 + "%");
+    }
+  });
+
+  Vue.component("search-screen", {
+    template: "#search-screen-template",
+    data: function() {
+      return { searchterm: "" };
     },
-    "magnet/:magnet_link/": function(params) {
-      params.magnet_link = decodeURIComponent(params.magnet_link);
-      params.magnet_link = decodeURIComponent(params.magnet_link);
-      init_elements();
-      $("table").show();
-      $(".loadingwrapper").show();
-      $.post("/api/magnet_files/", { magnet_link: params.magnet_link });
-      var get_files = function() {
-        $.get("/api/magnet/" + get_hash(params.magnet_link) + "/", function(
-          data
-        ) {
-          $("tbody").html("");
-          if (data.files == null) {
-            setTimeout(get_files, 1000);
-            return;
-          }
-          $(".loadingwrapper").hide();
-          data.files.forEach(function(file) {
-            var tr = $("<tr><td>" + file + "</a></td></tr>");
-            $(tr).click(function() {
-              router.navigate(
-                "/magnet/" +
-                  encodeURIComponent(encodeURIComponent(params.magnet_link)) +
-                  "/" +
-                  file
-              );
-            });
-            $("tbody").append(tr);
-          });
-          $(".loadingwrapper").hide();
-        }).fail(function() {
-          $(".loadingwrapper").hide();
-        });
-      };
-      get_files();
+    methods: {
+      onSubmit: function(e) {
+        e.preventDefault();
+        router.navigate("/search/" + this.searchterm);
+      }
+    }
+  });
+
+  Vue.component("search-results-screen", {
+    mixins: [rbmixin],
+    data: function() {
+      return { results: null };
     },
-    "magnet/:magnet_link/:filename": function(params) {
-      params.magnet_link = decodeURIComponent(params.magnet_link);
-      params.magnet_link = decodeURIComponent(params.magnet_link);
-      init_elements();
-      $(".filename")
-        .show()
-        .html(params.filename);
-      $.post("/api/magnet_download/", {
-        magnet_link: params.magnet_link,
-        filename: params.filename
+    template: "#search-results-screen-template",
+    created: function() {
+      var self = this;
+      $.get("/api/search/" + this.params.searchterm, function(data) {
+        self.results = data.results;
       });
-      var get_file_info = function() {
+    }
+  });
+
+  Vue.component("filelist-screen", {
+    mixins: [rbmixin],
+    data: function() {
+      return { results: null };
+    },
+    template: "#filelist-screen-template",
+    created: function() {
+      $.post("/api/magnet_files/", { magnet_link: this.params.magnet_link });
+      var self = this;
+      (function get_files() {
         $.get(
-          "/api/magnet/" + get_hash(params.magnet_link) + "/" + params.filename,
+          "/api/magnet/" + get_hash(self.params.magnet_link) + "/",
           function(data) {
-            var text = data.status.replace(/_/g, " ");
-            if (data.status === "downloading" || data.status == "converting") {
-              text = text + " (" + Math.round(data.progress * 100) + "%)";
-              $(".progress").show();
-              $(".progress-bar").width(data.progress * 100 + "%");
-            } else {
-              $(".peernum").hide();
-              $(".progress").hide();
-            }
-
-            if (data.status === "downloading") {
-              $(".peernum").html(data.peers + " Peers");
-              $(".peernum").show();
-            } else {
-              $(".peernum")
-                .html("")
-                .hide();
-            }
-            if (data.status == "ready") {
-              $(".loadingwrapper").hide();
-              $(".videolink")
-                .attr(
-                  "href",
-                  "/play/" +
-                    get_hash(params.magnet_link) +
-                    "/" +
-                    params.filename
-                )
-                .show();
+            if (data.files == null) {
+              setTimeout(get_files, 1000);
               return;
             }
-            $(".loadingtext").html(text);
-            setTimeout(get_file_info, 1000);
+            self.results = data.files;
           }
-        ).fail(function() {
-          $(".loadingwrapper").hide();
-        });
-      };
-      get_file_info();
-      $(".loadingwrapper").show();
-    },
-    "*": function() {
-      init_elements();
-      $("form").show();
-      $("input").focus();
+        );
+      })();
     }
-  })
-  .resolve();
+  });
 
-$("form").submit(function(event) {
-  router.navigate("/search/" + $("input").val());
-  event.preventDefault();
-});
+  Vue.component("download-screen", {
+    mixins: [rbmixin],
+    data: function() {
+      return {
+        progress: null,
+        status: null,
+        peers: null,
+        heading: "Loading",
+        subheading: null,
+        play_link: null
+      };
+    },
+    template: "#download-screen-template",
+    created: function() {
+      this.play_link =
+        "/play/" +
+        get_hash(this.params.magnet_link) +
+        "/" +
+        this.params.filename;
+      $.post("/api/magnet_download/", {
+        magnet_link: this.params.magnet_link,
+        filename: this.params.filename
+      });
+      var self = this;
+      (function get_file_info() {
+        $.get(
+          "/api/magnet/" +
+            get_hash(self.params.magnet_link) +
+            "/" +
+            self.params.filename,
+          function(data) {
+            self.status = data.status;
+            self.progress = data.progress;
+            var text = data.status.replace(/_/g, " ");
+            self.heading =
+              data.progress === 0 || data.progress
+                ? text + " (" + Math.round(data.progress * 100) + "%)"
+                : text;
+            self.subheading =
+              data.peers === 0 || data.peers ? data.peers + " Peers" : null;
+            if (self.status !== "ready") {
+              setTimeout(get_file_info, 1000);
+            }
+          }
+        );
+      })();
+    }
+  });
+
+  var vm = new Vue({
+    el: "#app",
+    data: { screen: null, params: {} }
+  });
+
+  function display_view(view_name) {
+    return function(params) {
+      if (params && params.magnet_link) {
+        params.magnet_link = decodeURIComponent(params.magnet_link);
+        params.magnet_link = decodeURIComponent(params.magnet_link);
+      }
+      vm.screen = view_name;
+      vm.params = params;
+    };
+  }
+
+  var router = new Navigo(window.location.origin);
+  router
+    .on({
+      "search/:searchterm": display_view("search-results-screen"),
+      "magnet/:magnet_link/": display_view("filelist-screen"),
+      "magnet/:magnet_link/:filename": display_view("download-screen"),
+      "*": display_view("search-screen")
+    })
+    .resolve();
+})();

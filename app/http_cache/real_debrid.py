b4e10d5fa0d2afc3b26df48f453ffcb1ea0bbfcb
hauxir <hauxir@gmail.com>
2024-07-06 21:06:16 +0000

    use a the secret token instead of client id and secret for real debrid

--- app/http_cache/real_debrid.py
+++ app/http_cache/real_debrid.py
@@ -4,35 +4,18 @@ from urllib.parse import unquote
 
 import log
 
-
-DEVICE_CODE = os.environ.get("RD_DEVICE_CODE")
-CLIENT_ID = os.environ.get("RD_CLIENT_ID")
-CLIENT_SECRET = os.environ.get("RD_CLIENT_SECRET")
-
+RD_TOKEN = os.environ.get("RD_TOKEN")
 
 def get_cached_url(magnet_hash, filename):
 
-    if not any([DEVICE_CODE, CLIENT_ID, CLIENT_SECRET]):
+    if not RD_TOKEN:
         return None
 
     try:
-
-        creds = requests.post(
-            "https://api.real-debrid.com/oauth/v2/token",
-            dict(
-                client_id=CLIENT_ID,
-                client_secret=CLIENT_SECRET,
-                code=DEVICE_CODE,
-                grant_type="http://oauth.net/grant_type/device/1.0",
-            ),
-        ).json()
-
-        access_token = creds["access_token"]
-
         def get(path):
             return requests.get(
                 f"https://api.real-debrid.com/rest/1.0{path}",
-                headers=dict(authorization=f"Bearer {access_token}"),
+                headers=dict(authorization=f"Bearer {RD_TOKEN}"),
             ).json()
 
         def post(path, data):
@@ -40,7 +23,7 @@ def get_cached_url(magnet_hash, filename):
                 return requests.post(
                     f"https://api.real-debrid.com/rest/1.0{path}",
                     data,
-                    headers=dict(authorization=f"Bearer {access_token}"),
+                    headers=dict(authorization=f"Bearer {RD_TOKEN}"),
                 ).json()
             except:
                 return None

8c821a21c3835c16850edb2a78b450ec65c0602e
Haukur Rósinkranz <hauxir@gmail.com>
2024-12-13 16:15:47 +0000

    Update real_debrid.py

--- app/http_cache/real_debrid.py
+++ app/http_cache/real_debrid.py
@@ -28,23 +28,19 @@ def get_cached_url(magnet_hash, filename):
             except:
                 return None
 
-        instant = get(f"/torrents/instantAvailability/{magnet_hash}")
-        result = instant.get(magnet_hash)
-        rd = result.get("rd") if isinstance(result, dict) else None
-        if rd:
-            result = post(
-                "/torrents/addMagnet/",
-                dict(magnet=f"magnet:?xt=urn:btih:{magnet_hash}"),
-            )
-            torrent_id = result["id"]
-            post(f"/torrents/selectFiles/{torrent_id}", dict(files="all"))
-            links = get(f"/torrents/info/{torrent_id}")["links"][:30]
-            unrestricted_links = [
-                post("/unrestrict/link", dict(link=link))["download"] for link in links
-            ]
-            for i, link in enumerate(unrestricted_links):
-                if unquote(link).endswith(filename):
-                    return link
+        result = post(
+            "/torrents/addMagnet/",
+            dict(magnet=f"magnet:?xt=urn:btih:{magnet_hash}"),
+        )
+        torrent_id = result["id"]
+        post(f"/torrents/selectFiles/{torrent_id}", dict(files="all"))
+        links = get(f"/torrents/info/{torrent_id}")["links"][:30]
+        unrestricted_links = [
+            post("/unrestrict/link", dict(link=link))["download"] for link in links
+        ]
+        for i, link in enumerate(unrestricted_links):
+            if unquote(link).endswith(filename):
+                return link
 
     except Exception as e:
         log.write_log()
